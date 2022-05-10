39d35e97046ece7cdeb084921433f101e2a0b962
xxyy <hxy912@gmail.com>
2022-05-10 12:58:03 +0800

    remove install in batch

--- MNIST.sh
+++ MNIST.sh
@@ -14,10 +14,10 @@ module load gcc/8.3.0 cuda/10.2 cudnn/7.6.4 nccl/2.4.8 tensorrt/6.0.1 openmpi/4.
 0.2 fftw/3.3.8 szip/2.1.1 ffmpeg/4.2.1 opencv/4.1.1 python/3.7
 
 # install python pkgs
-virtualenv --no-download $SLURM_TMPDIR/env
-source $SLURM_TMPDIR/env/bin/activate
-pip install --no-index --upgrade pip
-pip install --no-index -r requirements.txt
+# virtualenv --no-download $SLURM_TMPDIR/env
+# source $SLURM_TMPDIR/env/bin/activate
+# pip install --no-index --upgrade pip
+# pip install --no-index -r requirements.txt
 
 # run python jobs
 python MNIST.py
